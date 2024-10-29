edbf22e89547178fd2154b1873692f96ca29cd14
mardejesus <mardejesus001@gmail.com>
2024-10-29 00:53:17 -0300

    docker y application funcionando. Cambie configuraciones de docker y application.properties. Versiones no compatibles

--- src/main/kotlin/snippet/repositories/SnippetRepository.kt
+++ src/main/kotlin/snippet/repositories/SnippetRepository.kt
@@ -5,6 +5,4 @@ import org.springframework.stereotype.Repository
 import snippet.model.entities.Snippet
 
 @Repository
-interface SnippetRepository : CrudRepository<Snippet, String>{
-    fun getSnippetById(id: String): Snippet
-}
+interface SnippetRepository : CrudRepository<Snippet, String>
