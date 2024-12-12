ae745d5c94eef9746b2da5707d818078113551ef
zartf <tomas.alcazar@ing.austral.edu.ar>
2024-12-12 03:50:47 -0300

    rollback

--- src/main/kotlin/snippet/CORSConfig.kt
+++ src/main/kotlin/snippet/CORSConfig.kt
@@ -1,19 +1,14 @@
 package snippet.snippet
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class CORSConfig : WebMvcConfigurer {
-
-    @Value("\${FRONTEND_URI}")
-    private lateinit var frontendUrl: String
-
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // Match all endpoints
-            .allowedOrigins(frontendUrl) // Dynamically use the FRONTEND_URI environment variable
+            .allowedOrigins("http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow OPTIONS for preflight
             .allowedHeaders("*") // Allow all headers
             .allowCredentials(true) // Allow credentials
