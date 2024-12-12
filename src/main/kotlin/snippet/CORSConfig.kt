a6457b0f11eb53cd8e77f80c1810e5d2f1f64651
zartf <tomas.alcazar@ing.austral.edu.ar>
2024-12-12 01:48:13 -0300

    add cors for front end

--- src/main/kotlin/snippet/CORSConfig.kt
+++ src/main/kotlin/snippet/CORSConfig.kt
@@ -1,14 +1,19 @@
 package snippet.snippet
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class CORSConfig : WebMvcConfigurer {
+
+    @Value("\${FRONTEND_URI}")
+    private lateinit var frontendUrl: String
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // Match all endpoints
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins(frontendUrl) // Dynamically use the FRONTEND_URI environment variable
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow OPTIONS for preflight
             .allowedHeaders("*") // Allow all headers
             .allowCredentials(true) // Allow credentials
